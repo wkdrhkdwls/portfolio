39bb84ae8822b13cb0e7dcba35216bd1b8b89b19
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-06-10 17:43:51 +0900

    Design : 프로젝트 이미지가 불균형하게 배치된 문제 해결

--- src/components/Project/project.jsx
+++ src/components/Project/project.jsx
@@ -5,32 +5,29 @@ import { Link } from "gatsby"
 
 const Projects = () => {
   return (
-    <section class="project" id="portfolio">
-      <ul class="project-list">
+    <section className="project" id="portfolio">
+      <ul className="project-list">
         <li>
-          <div class="project-content section-content">
-            <p class="section-subtitle">My Works</p>
-
-            <h2 class="h3 section-title">Projects</h2>
-
-            <p class="section-text">프로젝트 목록</p>
+          <div className="project-content section-content">
+            <p className="section-subtitle">My Works</p>
+            <h2 className="h3 section-title">Projects</h2>
+            <p className="section-text">프로젝트 목록</p>
           </div>
         </li>
 
         <li>
-          <Link href="/projects/gganbu" class="project-card">
-            <figure class="card-banner">
+          <Link href="/projects/gganbu" className="project-card">
+            <figure className="card-banner">
               <StaticImage
                 src="../../../contents/images/gganbu/gganbuTitle.png"
-                class="w-100"
+                className="project-image"
                 alt="A macintosh on a yellow background."
               />
             </figure>
 
-            <div class="card-content">
-              <h3 class="h4 card-title">깐부GG</h3>
-
-              <time class="publish-date" datetime="2023-01">
+            <div className="card-content">
+              <h3 className="h4 card-title">깐부GG</h3>
+              <time className="publish-date" dateTime="2023-01">
                 2023.01
               </time>
             </div>
@@ -38,19 +35,18 @@ const Projects = () => {
         </li>
 
         <li>
-          <Link href="/projects/tmtp" class="project-card">
-            <figure class="card-banner">
+          <Link href="/projects/tmtp" className="project-card">
+            <figure className="card-banner">
               <StaticImage
                 src="../../../contents/images/tmtp/tmtp.jpg"
-                class="w-100"
+                className="project-image"
                 alt="A Cassette tape on a mellow apricot background."
               />
             </figure>
 
-            <div class="card-content">
-              <h3 class="h4 card-title">TMTP</h3>
-
-              <time class="publish-date" datetime="2022-04">
+            <div className="card-content">
+              <h3 className="h4 card-title">TMTP</h3>
+              <time className="publish-date" dateTime="2022-04">
                 2023.05
               </time>
             </div>
@@ -58,19 +54,18 @@ const Projects = () => {
         </li>
 
         <li>
-          <Link href="/projects/qrunch" class="project-card">
-            <figure class="card-banner">
+          <Link href="/projects/qrunch" className="project-card">
+            <figure className="card-banner">
               <StaticImage
                 src="../../../contents/images/qrunch/Qrunch.png"
-                class="w-50"
+                className="project-image"
                 alt="Blue digital watch on a dark liver background."
               />
             </figure>
 
-            <div class="card-content">
-              <h3 class="h4 card-title">Qrunch</h3>
-
-              <time class="publish-date" datetime="2023-09">
+            <div className="card-content">
+              <h3 className="h4 card-title">Qrunch</h3>
+              <time className="publish-date" dateTime="2023-09">
                 2023.09
               </time>
             </div>
@@ -78,76 +73,55 @@ const Projects = () => {
         </li>
 
         <li>
-          <Link href="/projects/mylist" class="project-card">
-            <figure class="card-banner">
+          <Link href="/projects/mylist" className="project-card">
+            <figure className="card-banner">
               <StaticImage
                 src="../../../contents/images/mylist/mylist.png"
-                class="myListBanner"
+                className="project-image"
                 alt="Blue digital watch on a dark liver background."
               />
             </figure>
 
-            <div class="card-content">
-              <h3 class="h4 card-title">My List</h3>
-
-              <time class="publish-date" datetime="2023-12">
+            <div className="card-content">
+              <h3 className="h4 card-title">My List</h3>
+              <time className="publish-date" dateTime="2023-12">
                 2023.12
               </time>
             </div>
           </Link>
         </li>
-        <li>
-          <Link href="/projects/mymatch" class="project-card">
-            <figure class="card-banner">
-              <StaticImage
-                src="../../../contents/images/mymatch/mymatch.png"
-                class="myMatchBanner"
-                alt="Blue digital watch on a dark liver background."
-              />
-            </figure>
 
-            <div class="card-content">
-              <h3 class="h4 card-title">MyMatch</h3>
-
-              <time class="publish-date" datetime="2024-03">
-                2024.03
-              </time>
-            </div>
-          </Link>
-        </li>
         <li>
-          <Link href="/projects/metlife" class="project-card">
-            <figure class="card-banner">
+          <Link href="/projects/metlife" className="project-card">
+            <figure className="card-banner">
               <StaticImage
                 src="../../../contents/images/metlife/metlife.jpg"
-                class="metLifeBanner"
+                className="project-image"
                 alt="Blue digital watch on a dark liver background."
               />
             </figure>
 
-            <div class="card-content">
-              <h3 class="h4 card-title">Metlife 해커톤</h3>
-
-              <time class="publish-date" datetime="2024-03">
+            <div className="card-content">
+              <h3 className="h4 card-title">Metlife 해커톤</h3>
+              <time className="publish-date" dateTime="2024-03">
                 2024.03
               </time>
             </div>
           </Link>
         </li>
         <li>
-          <Link href="/projects/duett" class="project-card">
-            <figure class="card-banner">
+          <Link href="/projects/duett" className="project-card">
+            <figure className="card-banner">
               <StaticImage
                 src="../../../contents/images/duett/duett.png"
-                class="metLifeBanner"
+                className="project-image"
                 alt="Blue digital watch on a dark liver background."
               />
             </figure>
 
-            <div class="card-content">
-              <h3 class="h4 card-title">Duett</h3>
-
-              <time class="publish-date" datetime="2024-03">
+            <div className="card-content">
+              <h3 className="h4 card-title">Duett</h3>
+              <time className="publish-date" dateTime="2024-06">
                 2024.06
               </time>
             </div>
