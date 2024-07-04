75e2a59d710473bdaac0eb7c4125fe07697535f9
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-07-04 10:15:22 +0900

    Chore : Update header component to handle project page and improve navigation

--- src/components/Layout/header.jsx
+++ src/components/Layout/header.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 import "../../styles/style.css"
 import { FaGripVertical, FaTimes } from "react-icons/fa"
 
 const Header = ({ siteTitle }) => {
   const [isActive, setIsActive] = useState(false)
+  const [isProjectPage, setIsProjectPage] = useState(false)
 
   const handleToggle = () => {
     setIsActive(!isActive)
   }
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsProjectPage(window.location.pathname.includes("project"))
+    }
+  })
 
   return (
     <div>
@@ -18,58 +25,61 @@ const Header = ({ siteTitle }) => {
             <Link to="/">{siteTitle}</Link>
           </h1>
 
-          <div className="navbar-actions">
-            <button
-              className="theme-btn"
-              aria-label="Change Theme"
-              title="Change Theme"
-              data-theme-btn
-            >
-              <span className="icon"></span>
-            </button>
-          </div>
+          {!isProjectPage && (
+            <>
+              {" "}
+              <div className="navbar-actions">
+                <button
+                  className="theme-btn"
+                  aria-label="Change Theme"
+                  title="Change Theme"
+                  data-theme-btn
+                >
+                  <span className="icon"></span>
+                </button>
+              </div>
+              <button
+                className={`nav-toggle-btn ${isActive ? "active" : ""}`}
+                aria-label="Toggle Menu"
+                title="Toggle Menu"
+                data-nav-toggle-btn
+                onClick={handleToggle}
+              >
+                {isActive ? (
+                  <FaTimes color="red" size={20} />
+                ) : (
+                  <FaGripVertical color="red" size={20} />
+                )}
+              </button>
+              <nav className="navbar" data-navbar>
+                <ul className="navbar-list">
+                  <li>
+                    <a href="#home" className="navbar-link">
+                      Home.
+                    </a>
+                  </li>
 
-          <button
-            className={`nav-toggle-btn ${isActive ? "active" : ""}`}
-            aria-label="Toggle Menu"
-            title="Toggle Menu"
-            data-nav-toggle-btn
-            onClick={handleToggle}
-          >
-            {isActive ? (
-              <FaTimes color="red" size={20} />
-            ) : (
-              <FaGripVertical color="red" size={20} />
-            )}
-          </button>
+                  <li>
+                    <a href="#about" className="navbar-link">
+                      About.
+                    </a>
+                  </li>
 
-          <nav className="navbar" data-navbar>
-            <ul className="navbar-list">
-              <li>
-                <a href="#home" className="navbar-link">
-                  Home.
-                </a>
-              </li>
+                  <li>
+                    <a href="#skills" className="navbar-link">
+                      Skills.
+                    </a>
+                  </li>
 
-              <li>
-                <a href="#about" className="navbar-link">
-                  About.
-                </a>
-              </li>
-
-              <li>
-                <a href="#skills" className="navbar-link">
-                  Skills.
-                </a>
-              </li>
-
-              <li>
-                <a href="#portfolio" className="navbar-link">
-                  Portfolio.
-                </a>
-              </li>
-            </ul>
-          </nav>
+                  <li>
+                    <a href="#portfolio" className="navbar-link">
+                      Portfolio.
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </>
+          )}
         </div>
       </header>
     </div>
