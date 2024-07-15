47384d500daeaff6944c29db326b11766238daa9
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-07-15 10:08:52 +0900

    chore: Update Duett page layout and styles

---
+++ src/pages/projects/duett.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import "../../styles/style.css"
+import CustomScript from "../../styles/custom"
+import Layout from "../../components/Layout/layout"
+import ArrowUp from "../../utils/arrow"
+
+const Duett = () => {
+  return (
+    <>
+      <Layout>
+        <main>
+          <article class="container">
+            <section class="project-detail" id="home">
+              null
+            </section>
+          </article>
+        </main>
+      </Layout>
+      <ArrowUp />
+
+      <CustomScript />
+    </>
+  )
+}
+
+export default Duett
